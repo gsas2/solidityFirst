b7364951458fc4b2a688d3621be465afb0314b8d
Gastón Sas <gsas@elementum.com>
2021-09-11 18:02:34 -0300

    Completing FE to read, update and display counter value using contract

--- counter-fe/src/App.js
+++ counter-fe/src/App.js
@@ -1,10 +1,33 @@
+import { useEffect, useState } from 'react';
+import { useWeb3, useContract } from './hooks/web3';
+import counterAbi from './contracts/counter.json';
+
+const provider = process.env.REACT_APP_PROVIDER;
+const account = process.env.REACT_APP_ACCOUNT;
+const counterAddress = process.env.REACT_APP_COUNTER_CONTRACT_ADDRESS;
+
 const App = () => {
-  const increaseHandler = () => {
-    console.log('Dale que sos bosch');
+  const web3 = useWeb3(provider, account);
+  const counterContract = useContract(web3, counterAbi, counterAddress);
+  const [counter, setCounter] = useState(0);
+
+
+  useEffect(() => {
+    const getCounterValue = async () => {
+      const counter = await counterContract.methods.counter().call();
+      setCounter(counter);
+    };
+    getCounterValue();
+  }, [counterContract]);
+
+  const increaseHandler = async () => {
+    await counterContract.methods.increase().send({ from: account });
+    setCounter(await counterContract.methods.counter().call());
   }
 
-  const decreaseHandler = () => {
-    console.log('no sos bosch?');
+  const decreaseHandler = async () => {
+    await counterContract.methods.decrease().send({ from: account });
+    setCounter(await counterContract.methods.counter().call());
   }
 
   return (
@@ -12,7 +35,7 @@ const App = () => {
       <h2>My First Smart Contract: Counter</h2>
       <button onClick={increaseHandler}>+</button>
       <button onClick={decreaseHandler}>-</button>
-      <div>Counter: </div>
+      <div>Counter: {counter}</div>
     </div>
   );
 }
